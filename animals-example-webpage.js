18c6d8b817d9c32e49c768371fed435b638ee48d
a-cameron <72467055+a-cameron@users.noreply.github.com>
2020-10-14 13:48:17 +0100

    Migrated all into one HTML doc

--- animals-example-webpage.js
+++ animals-example-webpage.js
@@ -42,18 +42,6 @@ filterButton.addEventListener('click', () => { // listen out for when the filter
   
   toggleimages( searchItem );                       
   
-  /* The long hand version of what is now below
-  
-  listItems.forEach( function( item ) {  // Then loop through the listItems ( the filter buttons )
-    let dataClass = item.getAttribute('data-class');  // grab the attribute data-class of each item and save it here
-    
-       if ( dataClass === searchItem ) {  // then check to see if dataClass === searchItem ( what the user wrote in the text field )
-             item.classList.add('active'); // if it is the same grab that item and add the class active to it
-        } else {                                        // if it does match then....
-             item.classList.remove('active'); // remove the class active from it
-        }
- */
-  
   listItems.forEach( ( item ) => {  
     let dataClass = item.getAttribute('data-class'); 
    
